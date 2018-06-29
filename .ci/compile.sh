07d2fda8b4588785a8ba017d87c8ab20ff25c27d
Trevor Bekolay <tbekolay@gmail.com>
2018-06-29 12:32:37 -0400

    Add CI scripts for compiling
    The scripts do the following:
    
    - Set up SSH and git configuration
    - Compile Python to shared object files
    - Commit those compiled files to master in the `nengo-loihi-compiled`
      repository and push.
    
    This required a more specific environment for the gitlab-runner,
    so I added a `setup.sh` script. There was also some SSH setup
    for the gitlab-runner user, but I will document that in the wiki.

---
+++ .ci/compile.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+NAME=$0
+COMMAND=$1
+NLDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
+
+function usage {
+    echo "usage: $NAME <command>"
+    echo
+    echo "  setup   Set up ssh agent and git configuration"
+    echo "  run     Compile Python to object files"
+    echo "  deploy  Commit compiled files and push to remote"
+    exit 1
+}
+
+if [[ "$COMMAND" == "setup" ]]; then
+    eval $(ssh-agent -s)
+    git config --global user.name "Applied Brain Research"
+    git config --global user.email "info@appliedbrainresearch.com"
+    git clone ssh://git@gl.appliedbrainresearch.com:36563/abr/nengo-loihi-compiled.git
+elif [[ "$COMMAND" == "run" ]]; then
+    python .ci/compile.py
+elif [[ "$COMMAND" == "deploy" ]]; then
+    NLCOMMIT=$(git rev-parse --short HEAD)
+    cd nengo-loihi-compiled
+    git add -A
+    git commit -m "Compiled nengo-loihi at $NLCOMMIT"
+    git push origin master
+else
+    if [[ -z "$COMMAND" ]]; then
+        echo "Command required"
+    else
+        echo "Command $COMMAND not recognized"
+    fi
+    echo
+    usage
+fi
---
+++ .ci/setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+conda config --set always_yes yes --set changeps1 no
+
+# Exit if the loihi env already exists
+if conda info --envs | grep loihi > /dev/null; then
+    return 0
+fi
+
+# Otherwise, set it up
+conda update --quiet conda
+conda create --quiet --name loihi python=3.5.5
+source activate loihi
+conda install --quiet cython matplotlib mkl numpy scipy
+pip install flake8 nengo pylint pytest
+source deactivate
