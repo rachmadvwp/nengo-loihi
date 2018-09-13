4e5b926089cc9efe5901f8e881086e270e38b689
Eric Hunsberger <erichuns@gmail.com>
2018-09-13 17:25:36 -0400

    Implemented new axons for Loihi

--- nengo_loihi/loihi_interface.py
+++ nengo_loihi/loihi_interface.py
@@ -363,16 +363,36 @@ def build_synapses(n2core, core, group, synapses, cx_idxs):
             )
 
 
-def build_axons(n2core, core, group, axons, cx_idxs):
+def build_axons(n2core, core, group, axons, cx_ids):
     tchip_idx, tcore_idx, tsyn_idxs = core.board.find_synapses(axons.target)
-    taxon_idxs = np.asarray(tsyn_idxs)[axons.target_inds]
     n2board = n2core.parent.parent
     tchip_id = n2board.n2Chips[tchip_idx].id
     tcore_id = n2board.n2Chips[tchip_idx].n2Cores[tcore_idx].id
-    assert axons.n_axons == len(cx_idxs) == len(taxon_idxs)
-    for i in range(axons.n_axons):
-        n2core.createDiscreteAxon(
-            cx_idxs[i], tchip_id, tcore_id, int(taxon_idxs[i]))
+
+    cx_idxs = np.arange(len(cx_ids))
+    spikes = axons.map_cx_spikes(cx_idxs)
+    for cx_id, spike in zip(cx_ids, spikes):
+        taxon_idx = spike.axon_id
+        taxon_id = int(tsyn_idxs[taxon_idx])
+        n_populations = axons.target.axon_populations(taxon_idx)
+        if n_populations == 1:
+            assert spike.atom == 0
+            n2core.createDiscreteAxon(cx_id, tchip_id, tcore_id, taxon_id)
+        else:
+            srcRelCxId = 0  # TODO: what is this needed for??
+            n2core.createPop16Axon(
+                spike.atom, srcRelCxId, cx_id, tchip_id, tcore_id, taxon_id)
+
+
+    # tchip_idx, tcore_idx, tsyn_idxs = core.board.find_synapses(axons.target)
+    # taxon_idxs = np.asarray(tsyn_idxs)[axons.target_inds]
+    # n2board = n2core.parent.parent
+    # tchip_id = n2board.n2Chips[tchip_idx].id
+    # tcore_id = n2board.n2Chips[tchip_idx].n2Cores[tcore_idx].id
+    # assert axons.n_axons == len(cx_idxs) == len(taxon_idxs)
+    # for i in range(axons.n_axons):
+    #     n2core.createDiscreteAxon(
+    #         cx_idxs[i], tchip_id, tcore_id, int(taxon_idxs[i]))
 
 
 def build_probe(n2core, core, group, probe, cx_idxs):
