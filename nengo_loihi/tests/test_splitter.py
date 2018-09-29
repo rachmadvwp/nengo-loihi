9042d86b6eeff64ac9079243087ddb0bd2233004
Trevor Bekolay <tbekolay@gmail.com>
2018-09-29 16:30:23 -0400

    WIP: debug keyword spotting example

--- nengo_loihi/tests/test_splitter.py
+++ nengo_loihi/tests/test_splitter.py
@@ -1,6 +1,8 @@
-import pytest
 import nengo
 import numpy as np
+import pytest
+
+import nengo_loihi
 
 
 @pytest.mark.parametrize("pre_dims", [1, 3])
@@ -42,3 +44,46 @@ def test_manual_decoders(
     assert np.mean(sim.data[pre_probe]) > 100
     # But that post has no activity due to the zero weights
     assert np.all(sim.data[post_probe] == 0)
+
+
+@pytest.mark.parametrize("pre_onchip, post_onchip", [
+    (True, True), (True, False), (False, True)
+])
+def test_n2n_transform_solver(
+        allclose, plt, rng, Simulator, pre_onchip, post_onchip):
+    with nengo.Network() as net:
+        nengo_loihi.add_params(net)
+        n_neurons = 10
+
+        pre = nengo.Ensemble(n_neurons, 1)
+        net.config[pre].on_chip = pre_onchip
+
+        weights = rng.uniform(-0.001, 0.001, size=(n_neurons, n_neurons))
+
+        # Neuron to neuron connection with NoSolver
+        post_solver = nengo.Ensemble(n_neurons, 1, seed=10)
+        net.config[post_solver].on_chip = post_onchip
+        nengo.Connection(pre, post_solver.neurons,
+                         function=lambda x: np.zeros(n_neurons),
+                         solver=nengo.solvers.NoSolver(weights.T))
+
+        # Neuron to neuron connection with transform
+        post_transform = nengo.Ensemble(n_neurons, 1, seed=10)
+        net.config[post_transform].on_chip = post_onchip
+        nengo.Connection(pre.neurons, post_transform.neurons, transform=weights)
+
+        probe_solver = nengo.Probe(post_solver, synapse=0.03)
+        probe_transform = nengo.Probe(post_transform, synapse=0.03)
+
+    with Simulator(net) as sim:
+        sim.run(0.1)
+
+    plt.plot(sim.trange(), sim.data[probe_solver] - sim.data[probe_transform])
+    plt.ylabel("Difference between NoSolver and transform")
+    plt.xlabel("Time (s)")
+
+    # Ensure post_solver and post_transform have the same params
+    assert np.all(sim.data[post_transform].gain == sim.data[post_solver].gain)
+    assert np.all(sim.data[post_transform].bias == sim.data[post_solver].bias)
+
+    assert allclose(sim.data[probe_solver], sim.data[probe_transform])
