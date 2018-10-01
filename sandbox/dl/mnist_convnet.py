3684d22b5504237f229aefd154e9207c9365353a
Eric Hunsberger <erichuns@gmail.com>
2018-10-01 13:39:06 -0400

    Back to medium-sized model

--- sandbox/dl/mnist_convnet.py
+++ sandbox/dl/mnist_convnet.py
@@ -233,18 +233,18 @@ layer_dicts = [
     # dict(layer_func=TfConv2d('layer3', 128, kernel_size=3, strides=2), neuron_type=neuron_type),
     # dict(layer_func=TfConv2d('layer4', 256, kernel_size=3, strides=2), neuron_type=neuron_type),
     # dict(layer_func=TfDense('layer_out', 10)),
-    # dict(layer_func=TfConv2d('layer1', 1, kernel_size=1, initializer=tf.constant_initializer(1)),
-    #      neuron_type=nengo.RectifiedLinear(), on_chip=False),
-    # # ^ Has to be one channel input for now since we can't send pop spikes to chip
-    # dict(layer_func=TfConv2d('layer2', 32, kernel_size=3, strides=2), neuron_type=neuron_type),
-    # dict(layer_func=TfConv2d('layer3', 64, kernel_size=3, strides=2), neuron_type=neuron_type),
-    # dict(layer_func=TfConv2d('layer4', 128, kernel_size=3, strides=2), neuron_type=neuron_type),
-    # dict(layer_func=TfDense('layer_out', 10)),
     dict(layer_func=TfConv2d('layer1', 1, kernel_size=1, initializer=tf.constant_initializer(1)),
          neuron_type=nengo.RectifiedLinear(amplitude=amp), on_chip=False, no_min_rate=True),
     # ^ Has to be one channel input for now since we can't send pop spikes to chip
-    dict(layer_func=TfConv2d('layer2', 16, kernel_size=3, strides=2), neuron_type=neuron_type),
+    dict(layer_func=TfConv2d('layer2', 32, kernel_size=3, strides=2), neuron_type=neuron_type),
+    dict(layer_func=TfConv2d('layer3', 64, kernel_size=3, strides=2), neuron_type=neuron_type),
+    dict(layer_func=TfConv2d('layer4', 128, kernel_size=3, strides=2), neuron_type=neuron_type),
     dict(layer_func=TfDense('layer_out', 10)),
+    # dict(layer_func=TfConv2d('layer1', 1, kernel_size=1, initializer=tf.constant_initializer(1)),
+    #      neuron_type=nengo.RectifiedLinear(amplitude=amp), on_chip=False, no_min_rate=True),
+    # # ^ Has to be one channel input for now since we can't send pop spikes to chip
+    # dict(layer_func=TfConv2d('layer2', 16, kernel_size=3, strides=2), neuron_type=neuron_type),
+    # dict(layer_func=TfDense('layer_out', 10)),
 ]
 
 # build the nengo_dl network
