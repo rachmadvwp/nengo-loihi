f18799dcc20cf59d12da8e27dd2d8fac2773fb02
Eric Hunsberger <erichuns@gmail.com>
2018-10-02 13:42:57 -0400

    Test for U/V overflow in emulator
    Compare emulator with Loihi to make sure it is the same.

--- nengo_loihi/tests/test_loihi_cx.py
+++ nengo_loihi/tests/test_loihi_cx.py
@@ -2,7 +2,8 @@ from nengo.exceptions import SimulationError
 import numpy as np
 import pytest
 
-from nengo_loihi.loihi_cx import CxGroup, CxModel, CxProbe, CxSimulator
+from nengo_loihi.loihi_cx import (
+    CxAxons, CxGroup, CxModel, CxProbe, CxSimulator, CxSpikeInput, CxSynapses)
 
 
 def test_simulator_noise(request, plt, seed):
@@ -52,3 +53,81 @@ def test_strict_mode():
     # Strict mode is a global setting so we set it back to True
     # for subsequent test runs.
     CxSimulator.strict = True
+
+
+@pytest.mark.skipif(pytest.config.getoption("--target") != "loihi",
+                    reason="need Loihi as comparison")
+@pytest.mark.parametrize('n_axons', [200, 1000])
+def test_uv_overflow(n_axons, Simulator, plt, allclose):
+    # TODO: Currently this is not testing the V overflow, since it is higher
+    #  and I haven't been able to figure out a way to make it overflow.
+
+    from nengo_loihi.loihi_api import VTH_MAX
+
+    # n_axons controls number of input spikes and thus amount of overflow
+    CxSimulator.strict = False
+
+    nt = 15
+
+    model = CxModel()
+
+    input_spikes = np.ones((nt, n_axons), dtype=bool)
+    input = CxSpikeInput(input_spikes)
+
+    group = CxGroup(1)
+    group.configure_relu()
+    group.configure_filter(0.1)
+    group.vmin = -2**22
+
+    synapses = CxSynapses(n_axons)
+    synapses.set_full_weights(np.ones((n_axons, 1)))
+
+    axons = CxAxons(n_axons)
+    axons.target = synapses
+    input.add_axons(axons)
+    group.add_synapses(synapses)
+
+    probe_u = CxProbe(target=group, key='u')
+    group.add_probe(probe_u)
+    probe_v = CxProbe(target=group, key='v')
+    group.add_probe(probe_v)
+    probe_s = CxProbe(target=group, key='s')
+    group.add_probe(probe_s)
+
+    model.add_input(input)
+    model.add_group(group)
+    model.discretize()
+
+    group.vth[:] = VTH_MAX  # must set after `discretize`
+
+    emu = model.get_simulator()
+    with pytest.warns(UserWarning):
+        emu.run_steps(nt)
+    emu_u = np.array(emu.probe_outputs[probe_u])
+    emu_v = np.array(emu.probe_outputs[probe_v])
+    emu_s = np.array(emu.probe_outputs[probe_s])
+
+    with model.get_loihi() as sim:
+        sim.run_steps(nt)
+        sim_u = np.column_stack([
+            p.timeSeries.data for p in sim.board.probe_map[probe_u]])
+        sim_v = np.column_stack([
+            p.timeSeries.data for p in sim.board.probe_map[probe_v]])
+        sim_s = np.column_stack([
+            p.timeSeries.data for p in sim.board.probe_map[probe_s]])
+        sim_v[sim_s > 0] = 0  # since Loihi has placeholder voltage after spike
+
+    plt.subplot(311)
+    plt.plot(emu_u)
+    plt.plot(sim_u)
+
+    plt.subplot(312)
+    plt.plot(emu_v)
+    plt.plot(sim_v)
+
+    plt.subplot(313)
+    plt.plot(emu_s)
+    plt.plot(sim_s)
+
+    assert allclose(emu_u[:-1], sim_u[1:])
+    assert allclose(emu_v[:-1], sim_v[1:])
