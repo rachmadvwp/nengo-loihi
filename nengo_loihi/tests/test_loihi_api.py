4a2f13758ba414227a00a867a9f413e39fcedc36
Eric Hunsberger <erichuns@gmail.com>
2018-10-15 12:43:39 -0400

    fixup! More accurate computation of decay magnitude

--- nengo_loihi/tests/test_loihi_api.py
+++ nengo_loihi/tests/test_loihi_api.py
@@ -7,7 +7,7 @@ from nengo_loihi.loihi_api import decay_magnitude, decay_int
 
 
 @pytest.mark.parametrize('offset', (0, 1))
-def test_decay_magnitude(offset, plt):
+def test_decay_magnitude(offset, plt, logger):
     bits = 12
     # decays = np.arange(1, 2**bits)
     decays = np.arange(1, 2**bits, 7)
@@ -40,10 +40,11 @@ def test_decay_magnitude(offset, plt):
         m0 = m.mean()
         emp.append(m0)
 
+        # reference (naive) method, not accounting for truncation loss
         r = (2**bits - offset - decay) / 2**bits
-        # Sx0 = 1. / (1 - r)  # naive method (decay to zero)
+        # Sx0 = 1. / (1 - r)  # sum_i^n x_i/x_0, where x_n = r**n*x_0 = 0
         # ref.append(Sx0)
-        Sx1 = (1 - r/x0) / (1 - r)  # naive method (decay to one)
+        Sx1 = (1 - r/x0) / (1 - r)  # sum_i^n x_i/x_0, where x_n = r**n*x_0 = 1
         ref.append(Sx1.mean())
 
         m = decay_magnitude(decay, x0, bits=bits, offset=offset)
@@ -53,8 +54,10 @@ def test_decay_magnitude(offset, plt):
     ref = np.array(ref)
     emp = np.array(emp)
     est = np.array(est)
-    # rms_ref = rms(ref - emp)
-    # rms_est = rms(est - emp)
+    rms_ref = rms(ref - emp) / rms(emp)
+    rms_est = rms(est - emp) / rms(emp)
+    logger.info("Ref rel RMSE: %0.3e, decay_magnitude rel RMSE: %0.3e" % (
+        rms_ref, rms_est))
 
     abs_ref = np.abs(ref - emp)
     abs_est = np.abs(est - emp)
