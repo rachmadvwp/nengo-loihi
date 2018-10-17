e70bf55801effe4d9097c459bef4714b6a29a64b
Eric Hunsberger <erichuns@gmail.com>
2018-10-17 15:37:55 -0400

    Simplify PES learning test and compare with Nengo
    - Simplified the PES learning test so that it no longer requires
      fitting, it just makes sure that the final learning output is near
      the target.
    - Added a comparison to Nengo to this test.

--- nengo_loihi/tests/test_learning.py
+++ nengo_loihi/tests/test_learning.py
@@ -9,6 +9,7 @@ def test_pes_comm_channel(allclose, plt, seed, Simulator, n_per_dim, dims):
     scale = np.linspace(1, 0, dims + 1)[:-1]
     input_fn = lambda t: np.sin(t * 2 * np.pi) * scale
 
+    tau = 0.01
     with nengo.Network(seed=seed) as model:
         stim = nengo.Node(input_fn)
 
@@ -19,7 +20,7 @@ def test_pes_comm_channel(allclose, plt, seed, Simulator, n_per_dim, dims):
         conn = nengo.Connection(
             pre, post,
             function=lambda x: np.zeros(dims),
-            synapse=0.01,
+            synapse=tau,
             learning_rule_type=nengo.PES(learning_rate=1e-3))
 
         nengo.Connection(post, conn.learning_rule)
@@ -29,34 +30,38 @@ def test_pes_comm_channel(allclose, plt, seed, Simulator, n_per_dim, dims):
         p_pre = nengo.Probe(pre, synapse=0.02)
         p_post = nengo.Probe(post, synapse=0.02)
 
-    with Simulator(model, precompute=False) as sim:
-        sim.run(5.0)
+    simtime = 5.0
+    with nengo.Simulator(model) as nengo_sim:
+        nengo_sim.run(simtime)
 
-    t = sim.trange()
+    with Simulator(model, precompute=False) as loihi_sim:
+        loihi_sim.run(simtime)
+
+    t = nengo_sim.trange()
+    pre_tmask = t > 0.1
+    post_tmask = t > simtime - 1.0
+
+    inter_tau = loihi_sim.model.inter_tau
+    y = nengo_sim.data[p_stim]
+    y_dpre = nengo.Lowpass(inter_tau).filt(y)
+    y_dpost = nengo.Lowpass(tau).combine(nengo.Lowpass(inter_tau)).filt(y_dpre)
+    y_nengo = nengo_sim.data[p_post]
+    y_loihi = loihi_sim.data[p_post]
+
     plt.subplot(211)
-    plt.plot(t, sim.data[p_stim])
-    plt.plot(t, sim.data[p_pre])
-    plt.plot(t, sim.data[p_post])
-
-    # --- fit input_fn to output, determine magnitude
-    #     The larger the magnitude, the closer the output is to the input
-    x = np.array([input_fn(tt)[0] for tt in t[t > 4]])
-    y = sim.data[p_post][t > 4][:, 0]
-    m = np.linspace(0, 1, 21)
-    errors = np.abs(y - m[:, None]*x).mean(axis=1)
-    m_best = m[np.argmin(errors)]
+    plt.plot(t, y_dpost, 'k', label='target')
+    plt.plot(t, y_nengo, 'b', label='nengo')
+    plt.plot(t, y_loihi, 'g', label='loihi')
 
     plt.subplot(212)
-    plt.plot(t[t > 4], x)
-    plt.plot(t[t > 4], y)
-    plt.plot(t[t > 4], m_best * x, ':')
+    plt.plot(t[post_tmask], y_loihi[post_tmask] - y_dpost[post_tmask], 'k')
+    plt.plot(t[post_tmask], y_loihi[post_tmask] - y_nengo[post_tmask], 'b')
 
-    assert allclose(sim.data[p_pre][t > 0.1],
-                    sim.data[p_stim][t > 0.1],
-                    atol=0.15,
-                    rtol=0.15)
-    assert np.min(errors) < 0.3, "Not able to fit correctly"
-    assert m_best > (0.3 if n_per_dim < 150 else 0.6)
+    assert allclose(loihi_sim.data[p_pre][pre_tmask], y_dpre[pre_tmask],
+                    atol=0.1, rtol=0.05)
+    assert allclose(y_loihi[post_tmask], y_dpost[post_tmask],
+                    atol=0.1, rtol=0.05)
+    assert allclose(y_loihi, y_nengo, atol=0.15, rtol=0.1)
 
 
 def test_multiple_pes(allclose, plt, seed, Simulator):
