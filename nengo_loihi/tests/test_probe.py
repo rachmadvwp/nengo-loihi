6e7534f55adb7a02c51686f0179459c41050cfd9
Eric Hunsberger <eric.hunsberger@appliedbrainresearch.com>
2019-01-17 10:44:58 -0500

    Test neuron current probe

--- nengo_loihi/tests/test_probe.py
+++ nengo_loihi/tests/test_probe.py
@@ -46,3 +46,39 @@ def test_repeated_probes(Simulator):
     for _ in range(5):
         with Simulator(net) as sim:
             sim.run(0.1)
+
+
+@pytest.mark.parametrize('precompute', [True, False])
+def test_neuron_current_probe(precompute, Simulator, seed, plt):
+    with nengo.Network(seed=seed) as model:
+        stim = nengo.Node(lambda t: [np.sin(t * 2 * np.pi)])
+
+        a = nengo.Ensemble(1, 1,
+                           encoders=nengo.dists.Choice([[1]]),
+                           max_rates=nengo.dists.Choice([100]),
+                           intercepts=nengo.dists.Choice([0]))
+        nengo.Connection(stim, a, synapse=None)
+
+        p_stim = nengo.Probe(stim, synapse=0.005)
+        p_current = nengo.Probe(a.neurons, 'input')
+
+        current_synapse = nengo.Alpha(0.01)
+        p_stim_f = nengo.Probe(
+            stim, synapse=current_synapse.combine(nengo.Lowpass(0.005)))
+        p_current_f = nengo.Probe(a.neurons, 'input', synapse=current_synapse)
+
+    with Simulator(model, precompute=precompute) as sim:
+        sim.run(1.0)
+
+    scale = 90000.  # empirically-measured scaling between input and current
+    x = sim.data[p_stim]
+    xf = sim.data[p_stim_f]
+    y = sim.data[p_current] / scale
+    yf = sim.data[p_current_f] / scale
+    plt.plot(sim.trange(), x)
+    plt.plot(sim.trange(), xf)
+    plt.plot(sim.trange(), y)
+    plt.plot(sim.trange(), yf)
+
+    assert np.allclose(y, x, atol=0.25, rtol=0)
+    assert np.allclose(yf, xf, atol=0.05, rtol=0)
